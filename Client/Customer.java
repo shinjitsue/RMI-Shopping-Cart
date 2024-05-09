4f4f06a7c418b1dd58f433710a3f32139b3c7a07
Shinjitsu <71762653+shinjitsue@users.noreply.github.com>
2024-05-09 20:15:39 +0800

    RMI

--- Client/Customer.java
+++ Client/Customer.java
@@ -5,53 +5,62 @@ import java.util.Scanner;
 public class Customer {
     public static void main(String[] args) {
         try {
-            // Locate the registry.
+            // Locate the registry
             Registry registry = LocateRegistry.getRegistry("127.0.0.1", 9100);
 
-            // Get the references of exported object from the RMI Registry...
+            // Get the references of exported object from the RMI Registry
             ProductInterface laptop = (ProductInterface) registry.lookup("laptop");
             ProductInterface mobilePhone = (ProductInterface) registry.lookup("mobilePhone");
             ProductInterface charger = (ProductInterface) registry.lookup("charger");
             ProductInterface powerBank = (ProductInterface) registry.lookup("powerBank");
 
+             // Create an array of products
+            ProductInterface[] products = {laptop, mobilePhone, charger, powerBank};
+
+            // Get the reference of exported Cart object from the RMI Registry
             CartInterface cart = (CartInterface) registry.lookup("access_cart");
 
             Scanner scanner = new Scanner(System.in);
             String userChoice;
-            do {
-                System.out.println("Enter the product you want to add to the cart: \n1. Laptop \n2. Mobile Phone \n3. Charger \n4. Power Bank \n5. View Cart \n6. Exit");
+            while (true) {
+                System.out.println();
+                System.out.println(" ---------------");
+                System.out.println("| Shopping Cart |");
+                System.out.println(" ---------------");
+                System.out.println("\n1. View Products");
+                System.out.println("2. View Cart");
+                System.out.println("3. Add Product to Cart");
+                System.out.println("4. Exit");
                 System.out.print("Enter your choice: ");
-                userChoice = scanner.nextLine();
+                int choice = scanner.nextInt(); 
+                System.out.println();
 
-                switch (userChoice) {
-                    case "1":
-                        cart.addProduct(laptop);
-                        System.out.println("Laptop is added to cart! \n");
+                switch (choice) {
+                    case 1:
+                        for (ProductInterface product : products) {
+                            System.out.println("---------------------------------------------");
+                            System.out.println(product.ViewProduct());
+                            System.out.println("---------------------------------------------");
+                        }
                         break;
-                    case "2":
-                        cart.addProduct(mobilePhone);
-                        System.out.println("Mobile Phone is added to cart! \n");
-                        break;
-                    case "3":
-                        cart.addProduct(charger);
-                        System.out.println("Charger is added to cart! \n");
-                        break;
-                    case "4":
-                        cart.addProduct(powerBank);
-                        System.out.println("Power Bank is added to cart! \n");
-                        break;
-                    case "5":
+                    case 2:
                         System.out.println(cart.viewAddedProducts());
                         break;
-                    case "6":
-                        System.out.println("Exiting...");
+                    case 3:
+                        System.out.println("Choose a product to add to the cart: ");
+                        for (int i = 0; i < products.length; i++) {
+                            System.out.println((i + 1) + ". " + products[i].getName());
+                        }
+                        System.out.print("Enter your choice: ");
+                        int productChoice = scanner.nextInt();
+                        cart.addProduct(products[productChoice - 1]);
                         break;
+                    case 4:
+                        System.exit(0);                      
                     default:
-                        System.out.println("Invalid choice. Please enter a valid option.");
-                        break;
+                        System.out.println("Invalid choice. Please enter a number between 1 and 4.");
                 }
-            } while (!userChoice.equals("6"));
-
+            }
         } catch (Exception e) {
             System.out.println("Client side error..." + e);
         }
--- Server/Server.java
+++ Server/Server.java
@@ -10,27 +10,33 @@ public class Server {
             System.setProperty("java.rmi.server.hostname", "127.0.0.1");
             System.out.println("Server has been started...");
 
+            // Creating Product Objects with the given details
             Product Laptop = new Product("Aspire", "Acer", "Laptop", 15, 3740.91, 4000.50);
             Product MobilePhone = new Product("Xen", "Chingchong", "MobilePhone", 5, 10233.82, 20565.20);
             Product Charger = new Product("LCharge","Lenovo Charger", "Charger", 8, 841.55, 1000.00);
             Product PowerBank = new Product("P1Bank","Panasonic", "PowerBank", 2, 44.12, 100.00);
             
+            // Exporting and binding of Objects to the Registry
             ProductInterface stub_laptop = (ProductInterface) UnicastRemoteObject.exportObject(Laptop, 0);
 			ProductInterface stub_mobilePhone = (ProductInterface) UnicastRemoteObject.exportObject(MobilePhone, 0);
             ProductInterface stub_charger = (ProductInterface) UnicastRemoteObject.exportObject(Charger, 0);
 			ProductInterface stub_powerBank = (ProductInterface) UnicastRemoteObject.exportObject(PowerBank, 0);
             
+            // Exporting and binding of Cart Object to the Registry
             CartInterface stub_cart = (CartInterface) UnicastRemoteObject.exportObject(cart, 0);
 
             Registry registry = LocateRegistry.getRegistry("127.0.0.1", 9100);
 
+            // Binding the Objects to the Registry
             registry.rebind("laptop", stub_laptop);
             registry.rebind("mobilePhone", stub_mobilePhone);
             registry.rebind("charger", stub_charger);
 			registry.rebind("powerBank", stub_powerBank);
             
+            // Binding the Cart Object to the Registry
             registry.rebind("access_cart", stub_cart);
 
+            // Displaying the Product details
             System.out.println(Laptop.ViewProduct()+ "\n");
             System.out.println(MobilePhone.ViewProduct()+ "\n");
             System.out.println(Charger.ViewProduct()+ "\n");
